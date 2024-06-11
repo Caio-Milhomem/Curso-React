b36c93d6d8fd845232380d38f2180f090216e900
Caio-Milhomem <caiomilhomem@outlook.com.br>
2024-06-11 13:00:46 -0300

    Props no Students

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Food from "./Food"
 import Card from "./Card"
 import Button from './assets/Button/Button'
 import ButtonInline from "./ButtonInline"
+import Student from "./Students"
 
 function App() {
 
@@ -13,6 +14,9 @@ function App() {
       <Button />
       <ButtonInline />
       <Header />
+      <Student name="Caio" age={24} isStudent={true} />
+      <Student name="Erika" age={25} isStudent={false} />
+      <Student />
       <Footer />
       <Food />
     </>
