236e872655825eefd4a0a02acd0db919a8cdad1d
Caio-Milhomem <caiomilhomem@outlook.com.br>
2024-06-21 12:59:53 -0300

    Add date to Digital Clock

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -1,32 +1,44 @@
 import { useEffect, useState } from "react";
 
 function DigitalClock() {
-
-    const [time, setTime] = useState(new Date())
+    const [time, setTime] = useState(new Date());
 
     useEffect(() => {
-
         const intervalId = setInterval(() => {
-            setTime(new Date())
-        }, 1000)
-
+            setTime(new Date());
+        }, 1000);
         return () => {
-            clearInterval(intervalId)
-        }
-
-    }, [])
+            clearInterval(intervalId);
+        };
+    }, []);
 
     function formatTime() {
-        let hours = time.getHours()
-        const min = time.getMinutes()
-        const sec = time.getSeconds()
-        const meridiem = hours >= 12 ? "PM" : "AM"
+        let hours = time.getHours();
+        const min = time.getMinutes();
+        const sec = time.getSeconds();
+        const meridiem = hours >= 12 ? "PM" : "AM";
+        //if (hours > 12) hours -= 12; // Convert 24-hour format to 12-hour format
+        return `${padZero(hours)}:${padZero(min)}:${padZero(sec)} ${meridiem}`;
+    }
+    function formatDate() {
+        const day = padZero(time.getDate());
+        const monthNames = [
+            "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+            "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+        ];
+        const dayNames = [
+            "Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira",
+            "Quinta-feira", "Sexta-feira", "Sábado"
+        ];
+        const dayOfWeek = dayNames[time.getDay()];
+        const month = monthNames[time.getMonth()];
+        const year = time.getFullYear();
 
-        return `${padZero(hours)}:${padZero(min)}:${padZero(sec)} ${meridiem}`
+        return `${dayOfWeek}, ${day} de ${month} de ${year}`;
     }
 
     function padZero(number) {
-        return (number < 10 ? "0" : "") + number
+        return (number < 10 ? "0" : "") + number;
     }
 
     return (
@@ -34,8 +46,11 @@ function DigitalClock() {
             <div className="clock">
                 <span>{formatTime()}</span>
             </div>
+            <div className="date">
+                <span>{formatDate()}</span>
+            </div>
         </div>
-    )
+    );
 }
 
-export default DigitalClock
+export default DigitalClock;
